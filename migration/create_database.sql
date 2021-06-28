c87f2bd4e58b75035d6121318a8b2fa6a117aeca
Tim Schraml <schramltim@gmail.com>
2021-06-28 22:27:06 +0200

    Fix wrong database name and add composer to docker-compose.yml

--- migration/create_database.sql
+++ migration/create_database.sql
@@ -1,7 +1,7 @@
 SELECT 'CREATE DATABASE taberu'
     WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'taberu')\gexec
 
-\c wardrobe;
+\c taberu;
 
 create user test with encrypted password 'test';
 grant all privileges on database taberu to test;
